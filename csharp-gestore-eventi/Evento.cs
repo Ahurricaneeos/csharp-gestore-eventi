a4e0d85d0c4b71da15d3cd80f130bc49616bbcf8
Ahurricaneeos <aurora_sala@hotmail.it>
2023-04-28 10:13:57 +0200

    Added getters and setters

--- csharp-gestore-eventi/Evento.cs
+++ csharp-gestore-eventi/Evento.cs
@@ -12,5 +12,37 @@ namespace csharp_gestore_eventi
         public DateTime date;
         public int capacity;
         public int places;
+
+        public Evento (string title, DateTime date, int capacity, int places)
+        {
+            SetTitle(title);
+            SetDate(date);
+            SetCapacity(capacity);
+            this.places = 0;
+        }
+          
+        public string GetTitle() { return title; }
+        public DateTime GetDate() { return date; }
+        public int GetCapacity() { return capacity; }
+        public int GetPlaces() { return places;}
+
+        private void SetTitle(string title)
+        {
+            if (title == "")
+            { throw new Exception("Il campo titolo è vuoto"); }
+            this.title = title;
+        }
+        private void SetDate(DateTime date)
+        {
+            if (date < DateTime.Now)
+            { throw new Exception("La data inserita è passata"); }
+            this.date = date;
+        }
+        private void SetCapacity(int capacity)
+        {
+            if (capacity < 0)
+            { throw new Exception("La capacità inserita è un numero negativo"); }
+            this.capacity = capacity;
+        }
     }
 }
