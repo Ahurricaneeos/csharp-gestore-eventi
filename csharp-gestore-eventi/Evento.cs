0e71761076e715292798a6fc1f82db602ea2d2c6
Ahurricaneeos <aurora_sala@hotmail.it>
2023-04-28 10:53:41 +0200

    Milestone 1

--- csharp-gestore-eventi/Evento.cs
+++ csharp-gestore-eventi/Evento.cs
@@ -44,5 +44,30 @@ namespace csharp_gestore_eventi
             { throw new Exception("La capacità inserita è un numero negativo"); }
             this.capacity = capacity;
         }
+
+        public void ReservePlaces(int reservedPlaces)
+        {
+            if (date < DateTime.Now)
+            { throw new Exception("L'evento è passato"); }
+            if (places < 0)
+            { throw new Exception("L'evento non ha posti"); }
+            if (places + reservedPlaces > capacity)
+            { throw new Exception("L'evento non ha abbastanza posti"); }
+            places += reservedPlaces;
+        }
+
+        public void CancelPlaces(int cancelledPlaces)
+        {
+            if (date < DateTime.Now)
+            { throw new Exception("L'evento è passato"); }
+            if (places - cancelledPlaces < 0)
+            { throw new Exception("I posti disdetti sono più di quelli prenotati"); }
+            places -= cancelledPlaces;
+        }
+
+        public override string ToString()
+        {
+            return date.ToString("dd/MM/yyyy") + " - " + title;
+        }
     }
 }
