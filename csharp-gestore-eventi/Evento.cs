2b4617549de991e0b807d8076e0a62e6c4538928
Ahurricaneeos <aurora_sala@hotmail.it>
2023-04-28 09:53:06 +0200

    Aggiungere i file di progetto.

---
+++ csharp-gestore-eventi/Evento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class Evento
+    {
+        public string title;
+        public DateTime date;
+        public int capacity;
+        public int places;
+    }
+}
